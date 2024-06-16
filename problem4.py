bb59a1aefa632efc8051de41727f72ef3166e737
fly <1756339062@qq.com>
2024-06-16 15:42:35 +0800

    problem4 finished

--- problem4.py
+++ problem4.py
@@ -28,8 +28,59 @@ def handle_input() -> list[tuple[int, int]]:
     return res
 
 
-def main():
-    res=handle_input()
+def main() -> list[int]:
+    res = handle_input()
+    n = len(res)
+    res.sort(key=lambda x: x[1])
+    maxDay = res[-1][-1]
+    minDay = 2*(10**4)
+    for i in range(len(res)):
+        if res[i][0] < minDay:
+            minDay = res[i][0]
+        if minDay == 1:
+            break
+
+    table: dict[int, list[int]] = dict()
+    for idx, duration in enumerate(res):
+        for d in range(duration[0], duration[1]+1):
+            if d not in table:
+                table[d] = []
+            table[d].append(idx)
+    ans: list[int] = []
+    # n lines
+    for k in range(1, n+1):
+        num = 0
+        # j denotes starting day
+        for j in range(minDay, maxDay-k+2):
+            invited: set[int] = set()
+            # k consecutive days starting from day j. Each day is denoted by day i
+            for i in range(j, j+k):
+                flag = False
+                if i not in table:
+                    break
+                avaliable = table[i]
+                for d in avaliable:
+                    if d not in invited:
+                        invited.add(d)
+                        flag = True
+                        break
+
+                # this means that we can not hold a conference from j to j+k because of day i
+                if not flag:
+                    break
+            else:
+                # this meas that we can hold a conference from j to j+k
+                num += 1
+
+        ans.append(num)
+
+    return ans
+
 
 if __name__ == '__main__':
-    main()
+    ans = main()
+    for i in range(len(ans)):
+        print(ans[i])
+
+# total time: 1:21:54.75
+# coding time: 36min

31e27190309b9870eacbc19a38acbcb4d7c74338
fly <1756339062@qq.com>
2024-06-18 20:53:50 +0800

    fix(P3): change to dp

--- problem3.py
+++ problem3.py
@@ -1,5 +1,8 @@
+from functools import reduce
+
+
 def handle_input() -> str:
-    input_string = input()
+    input_string = input().rstrip()
     if not input_string.isdigit():
         raise Exception('Invalid input')
     if len(input_string) > 50:
@@ -10,26 +13,55 @@ def handle_input() -> str:
 
 class Solution:
     def __init__(self) -> None:
-        self.string = handle_input()
-        self.num = 0
+        self.string = ""
 
     def getNum(self) -> int:
-        for i in range(0, 10):
-            self.auxFunc(2, i)
+        self.string = handle_input()
+        dp: list[list[int]] = [[0] * len(self.string) for _ in range(10)]
+        for i in range(10):
+            dp[i][0] = 1
 
-        return self.num
+        for i in range(1, len(dp[0])):
+            for j in range(10):
+                temp = 2*j
+                s_i = int(self.string[i])
+                if (temp+1-s_i >= 0 or temp-s_i >= 0 or temp-1-s_i >= 0):
+                    g_prev = temp-s_i
+                    if (g_prev >= 0 and g_prev < 10):
+                        dp[j][i] += dp[g_prev][i-1]
 
-    def auxFunc(self, i: int, g_prev: int):
-        if i > len(self.string):
-            self.num += 1
-            return
+                    g_prev += 1
+                    if (g_prev >= 0 and g_prev < 10):
+                        dp[j][i] += dp[g_prev][i-1]
 
-        temp = int(self.string[-1*i]) + g_prev
-        self.auxFunc(i+1, temp//2)
-        if temp % 2 != 0:
-            self.auxFunc(i+1, temp//2+1)
+                    g_prev -= 2
+                    if (g_prev >= 0 and g_prev < 10):
+                        dp[j][i] += dp[g_prev][i-1]
+
+        # for it in dp:
+        #     print(it)
+
+        ans = sum([sub[-1] for sub in dp])
+        # check
+        if len(self.string) > 2:
+            for i in range(1, len(self.string)):
+                g_i = int(self.string[-i])
+                if dp[g_i][-i] == 0:
+                    break
+
+                g_prev = int(self.string[-(i+1)])
+                temp = g_prev+g_i
+                if temp % 2 != 0 and (temp+1)//2 != g_i and temp//2 != g_i:
+                    break
+                elif temp % 2 == 0 and temp//2 != g_i:
+                    break
+            else:
+                ans -= 1
+
+        return ans
 
 
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.getNum())
+    ans = sol.getNum()
+    print(ans)
